47e774c804f7291b0f2d5acc23bd4d0e69270bf3
germano84-sys <germano84@gmail.com>
2026-04-21 14:20:08 -0500

    Refactor listar_carpetas function to return a list of directories instead of printing them --Rafael Espinosa

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
+# Cambios Rafael Espinosa
 import os
 
 def listar_carpetas(ruta):
     try:
-        print(f"\n📂 Carpetas en: {ruta}\n")
+        carpetas = []  # lista donde guardamos carpetas
+
         for elemento in os.listdir(ruta):
             ruta_completa = os.path.join(ruta, elemento)
             if os.path.isdir(ruta_completa):
-                print(f"- {elemento}")
+                carpetas.append(elemento)
+
+        return carpetas
+
     except Exception as e:
         print(f"Error: {e}")
+        return []
 
 if __name__ == "__main__":
     ruta = "D:\\"
-    listar_carpetas(ruta)
+    lista_carpetas = listar_carpetas(ruta)
+
+    print("\n📂 Lista de carpetas en el Ambito de Gestion DTIC:\n")
+    print(lista_carpetas)
